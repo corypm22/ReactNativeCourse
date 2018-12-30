0f86350f7247a0d7c053c8d5bcbbf1bdc795f8bf
Cory Palacios <Cory@Carloss-MBP.hitronhub.home>
2018-12-29 17:47:48 -0800

    rollback to before image #46

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -3,26 +3,15 @@ import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image} = album;
-  const {
-    thumbnailStyle,
-    headerContentStyle,
-    thumbnailContainerStyle,
-    headerTextStyle
-  } = styles;
+const AlbumDetail = (props) => {
+  
   return (
     <Card>
       <CardSection>
-        <View style={thumbnailContainerStyle}>
-          <Image
-            style={thumbnailStyle}
-            source={ { uri: thumbnail_image} }
-          />
-        </View>
-        <View style={headerContentStyle}>
-          <Text style={headerTextStyle}>{title}</Text>
-          <Text>{artist}</Text>
+        <View></View>
+        <View style={styles.headerContentStyle}>
+          <Text>{props.album.title}</Text>
+          <Text>{props.album.artist}</Text>
         </View>
       </CardSection>
     </Card>
@@ -30,23 +19,7 @@ const AlbumDetail = ({ album }) => {
 };
 
 const styles = {
-  headerContentStyle: {
-    flexDirection: 'column',
-    justifyContent: 'space-around'
-  }/*,
-  headerTextStyle: {
-    fontSize: 18
-  },
-  thumbnailStyle:{
-    height: 50,
-    width: 50
-  },
-  thumbnailContainerStyle: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginLeft: 10,
-    marginRight: 10,
-  }*/
+  
 
 };
 
