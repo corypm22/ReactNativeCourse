5d7cd43cf14e52e1ecd45fc81d116490b7f9002f
Cory Palacios <Cory@stackoverflow.com>
2019-01-10 14:02:49 -0800

    change to logout screen when logedin

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import {View } from 'react-native';
 import firebase from 'firebase';
-import { Header} from './components/common';
+import { Header, Button, CardSection} from './components/common';
 import LoginForm from './components/LoginForm'
 
 
 class App extends Component{
+    state = { loggedIn: false };
     componentWillMount(){
         firebase.initializeApp({
             apiKey: 'AIzaSyBktoMUK0eVMQvIyb765jhVsiUgFm7E73M',
@@ -15,13 +16,35 @@ class App extends Component{
             storageBucket: 'auth-66c4f.appspot.com',
             messagingSenderId: '287945174010'  
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn:true});
+            } else {
+                this.setState({ loggedIn : false});
+            }
+        });
+    }
+
+    renderContent() {
+        if(this.state.loggedIn) {
+            return (
+                <CardSection>
+                    <Button>
+                        Log Out
+                    </Button>
+                </CardSection> 
+            );
+        }
+
+        return <LoginForm/>;
     }
 
     render() {
         return (
             <View>
                 <Header headerText='Authentication'/>
-                <LoginForm/>
+                {this.renderContent()}
             </View>
         );
     }
